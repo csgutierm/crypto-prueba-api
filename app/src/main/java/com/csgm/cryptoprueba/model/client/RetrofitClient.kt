c1e0d9f5a2694dd1ab5bb78d1e43d02f3e78e2a9
csgutierm <csgutier@gmail.com>
2023-10-08 15:04:53 -0300

    Client

---
+++ app/src/main/java/com/csgm/cryptoprueba/model/client/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.csgm.cryptoprueba.model.client
+
+import com.csgm.cryptoprueba.model.remote.CryptoAPI
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://api.coincap.io/v2/"
+class RetrofitClient {
+    companion object {
+        fun instance(): CryptoAPI {
+            val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+                GsonConverterFactory.create()).build()
+            return retrofit.create(CryptoAPI::class.java)
+        }
+    }
+}
