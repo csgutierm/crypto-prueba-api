8b902fa243e71cc9c9ff69172408e14ccef78ceb
csgutierm <csgutier@gmail.com>
2023-10-08 14:35:52 -0300

    Lista de tareas

--- app/src/main/java/com/csgm/cryptoprueba/MainActivity.kt
+++ app/src/main/java/com/csgm/cryptoprueba/MainActivity.kt
@@ -3,6 +3,19 @@ package com.csgm.cryptoprueba
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/** LISTA DE TAREAS
+ * [ ] consumo de API
+ *      [ ] dependencias de retrofit
+ *      [ ] permiso de internet
+ *      [ ] POJOs
+ *      [ ] interfaz de operaciones
+ *      [ ] cliente de conexión con la API
+ * [ ] Repositorio
+ * [ ] ViewModel
+ * [ ] Fragmento de listado
+ * [ ] adeaptador + viewholder + recycleview
+ * [ ] persistencia de datos con ROOM
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
